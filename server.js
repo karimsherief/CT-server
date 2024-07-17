c626029f3faa66006e09181a3c43ce2dd9cdccbb
karim <sheriefkarim@gmail.com>
2024-07-17 12:01:03 +0300

    path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./data/db.json");
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000;
 const cors = require("cors");
